d776ee7fe6978d2a0d2334b3080a8a78c7cb4aac
adescoteaux1 <descoteaux.a@northeastern.edu>
2023-04-19 08:50:21 -0400

    edits

--- flask-app/src/customers/customers.py
+++ flask-app/src/customers/customers.py
@@ -178,10 +178,8 @@ def update_customer_address(customerID):
 @customers.route('/customer/products', methods=['GET'])
 def get_products():
     cursor = db.get_db().cursor()
-    cursor.execute('''
-    select product_name, descr, picture, unitPrice, first_name, last_name
-    from Products join Sellers
-    ''')
+    cursor.execute('select product_name, descr, picture, unitPrice, first_name, last_name\
+    from Products join Sellers')
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
